f916b19db80d53b8001e2f17e614913b45bee564
JosueML <josueml1905@gmail.com>
2026-01-09 15:43:06 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://josueml.github.io",
+    //site: "https://josueml.github.io",
     base: "/homekids/",
     vite: {
         plugins: [tailwindcss()],
